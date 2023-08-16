dbac4e4da075dcabacf102ef893bec8b81c8bcd0
Felpzcv <felipeviceconti@gmail.com>
2023-08-16 20:18:41 +0000

    arquivo gasolina.png atualizado

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# da-ebac
+# Criação e visualização de dados sobre gasolina em São Paulo
+Com esse código foi criado um gráfico a partir de um arquivo '.csv'. O projeto para visualização, demonsta a variação de preço ao decorrer dos primeiros 10 dias de Julho de 2021
--- grafico.py
+++ grafico.py
@@ -7,7 +7,7 @@ df_gasolina = pd.read_csv("gasolina.csv")
 df_gasolina.rename(columns={'dia': 'Dias', 'venda': 'Preços'}, inplace=True)
 
 
-graf_linha = sns.lineplot(data=df_gasolina, x="Dias", y="Preços", marker='o', markerfacecolor='blue', markeredgecolor='blue')
+graf_linha = sns.lineplot(data=df_gasolina, x="Dias", y="Preços", marker='o', markerfacecolor='black', markeredgecolor='black')
 graf_linha.lines[0].set_color("red")
 graf_linha.set_title("Preço da gasolina ao decorrer de 10 dias")
 plt.savefig("gasolina.png")
